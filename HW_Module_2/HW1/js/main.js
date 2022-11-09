ea8e9da9297fd8f7bbbd3df24cfd83983fe8f3ad
IgorMilya <78513411+IgorMilya@users.noreply.github.com>
2022-11-09 02:21:17 +0200

    HW1 done

---
+++ HW_Module_2/HW1/js/main.js
@@ -0,0 +1,27 @@
+let name, admin;
+name = 'Ihor';
+admin = name;
+console.log(admin);
+
+let days = Math.floor(Math.random() * 10) + 1;
+console.log(days + 's')
+
+let property = prompt("Enter some value");
+console.log(property);
+Boolean(2);
+
+// 1. Як можна оголосити змінну у Javascript?
+// 2. У чому різниця між функцією prompt та функцією confirm?
+// 3. Що таке неявне перетворення типів? Наведіть один приклад.
+
+//1. let, const, var
+//2. У функцию prompt мы вводим какое-то значение, а в функции confirm мы можем выбрать только OK / Cancel
+//   и эти значения будут как true and false. Также в confirm мы что-то подтверждаем.
+//3. Неявне перетворення это значения которые могут быть конвертированы между различными типами автоматически.
+// String(123) - явне
+// console.log(123 + '') - неявне
+
+
+
+
+
