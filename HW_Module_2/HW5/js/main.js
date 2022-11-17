bd50af73e15b28cb86962a0db68de40ab2b0c752
IgorMilya <78513411+IgorMilya@users.noreply.github.com>
2022-11-17 15:10:25 +0200

    HW5 done

--- HW_Module_2/HW5/js/main.js
+++ HW_Module_2/HW5/js/main.js
@@ -1,16 +1,15 @@
 const createNewUser = () => {
     let name = prompt("Enter your name");
     let lastName = prompt("Enter your lastName");
-    let newUser;
-    newUser = {
+    const newUser = {
         firstName: name,
         lastName: lastName,
-        getLogin(name,lastName) {
-            return  (name[0] + lastName).toLowerCase();
+        getLogin(name, lastName) {
+            return (name[0] + lastName).toLowerCase();
             // return  (this.firstName[0] + this.lastName).toLowerCase();
         }
     };
-    return newUser.getLogin(name,lastName);
+    return newUser.getLogin(name, lastName);
 }
 
 console.log(createNewUser());
