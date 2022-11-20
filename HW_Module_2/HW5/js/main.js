0624b14b2faadc979a1ec9680acaa4737ed00b66
IgorMilya <78513411+IgorMilya@users.noreply.github.com>
2022-11-20 19:06:54 +0200

    HW5 done

--- HW_Module_2/HW5/js/main.js
+++ HW_Module_2/HW5/js/main.js
@@ -1,15 +1,26 @@
 const createNewUser = () => {
     let name = prompt("Enter your name");
-    let lastName = prompt("Enter your lastName");
+    let secondName = prompt("Enter your lastName");
+
     const newUser = {
         firstName: name,
-        lastName: lastName,
-        getLogin(name, lastName) {
-            return (name[0] + lastName).toLowerCase();
-            // return  (this.firstName[0] + this.lastName).toLowerCase();
+        lastName: secondName,
+        writable: false,
+        getLogin() {
+            return (this.firstName[0] + this.lastName).toLowerCase();
         }
     };
-    return newUser.getLogin(name, lastName);
+
+    return newUser.getLogin();
+
 }
 
 console.log(createNewUser());
+
+//1. Опишіть своїми словами, що таке метод об'єкту
+//2. Який тип даних може мати значення властивості об'єкта?
+//3. Об'єкт це посилальний тип даних. Що означає це поняття?
+
+//1. Метод об'єкту это функция которая используеться в объекте и которая использует значения(данный) объекта
+//2. Любой тип данных котрому пренадлежит объекту
+//3. Это специальный внутрешний тип, который передаёт информацию
