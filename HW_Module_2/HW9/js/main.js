5a7e9317c7c930b66ca68a17df01cf4486c1f6b3
IgorMilya <78513411+IgorMilya@users.noreply.github.com>
2022-12-16 21:27:42 +0200

    Added theory of HW9

--- HW_Module_2/HW9/js/main.js
+++ HW_Module_2/HW9/js/main.js
@@ -21,6 +21,37 @@ const insert = (array, parent) => {
 
 insert(["Kharkiv", "Kyiv", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"], ul);
 
-setInterval(()=> ul.remove(), 3000);
+setInterval(() => ul.remove(), 3000);
 
 // setInterval(demolish,3000);
+
+
+//1. Опишіть, як можна створити новий HTML тег на сторінці.
+//2. Опишіть, що означає перший параметр функції insertAdjacentHTML і опишіть можливі варіанти цього параметра.
+//3. Як можна видалити елемент зі сторінки?
+
+// 1. Потрібно написати: document.createElement(' "some tag" ');
+
+// 2. Перший параметр це позиція відносто елемента. Можливо такі варіанти: "beforebegin", "afterbegin", "beforeend",
+//    "afterend". Beforebegin - перед елементом, afterbegin - перед першим дочірним елементом, beforeend - після
+//     останнього дочірнього елемента, beforeend - після елемента, але він дійсний якщо елемент має батьківський елемент.
+
+// 3. Потрібно написати: element.remove()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
