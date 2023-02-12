6bc526f5fdcc2212a65fc49e72da3fb61ac5c6a5
STRIX <imilay11@gmail.com>
2023-02-12 17:10:13 +0200

    Preparation for passing hw2

--- HW_Module_4/HW2/main.js
+++ HW_Module_4/HW2/main.js
@@ -1,72 +1,71 @@
-// const books = [
-//     {
-//         author: "Люсі Фолі",
-//         name: "Список запрошених",
-//         price: 70
-//     },
-//     {
-//         author: "Сюзанна Кларк",
-//         name: "Джонатан Стрейндж і м-р Норрелл",
-//     },
-//     {
-//         name: "Дизайн. Книга для недизайнерів.",
-//         price: 70
-//     },
-//     {
-//         author: "Алан Мур",
-//         name: "Неономікон",
-//         price: 70
-//     },
-//     {
-//         author: "Террі Пратчетт",
-//         name: "Рухомі картинки",
-//         price: 40
-//     },
-//     {
-//         author: "Анґус Гайленд",
-//         name: "Коти в мистецтві",
-//     }
-// ];
-//
-// let root = document.querySelector("#root");
-// let ul = document.createElement("ul");
-// root.append(ul);
-//
-//
-// books.map(item => {
-//
-//     const {author, name, price} = item;
-//
-//     if (!!author && !!name && !!price) {
-//         let ul = document.createElement("ul");
-//         ul.innerHTML = `
-//        <li>${author}</li>
-//        <li>${name}</li>
-//        <li>${price}</li>
-//        `
-//         root.append(ul);
-//     }
-//
-//     try {
-//         if (!author || !name || !price) {
-//             throw new Error("The property is not in obj")
-//         }
-//
-//     } catch (e) {
-//         console.error(e.message)
-//     }
-//
-// })
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+const books = [
+    {
+        author: "Люсі Фолі",
+        name: "Список запрошених",
+        price: 70
+    },
+    {
+        author: "Сюзанна Кларк",
+        name: "Джонатан Стрейндж і м-р Норрелл",
+    },
+    {
+        name: "Дизайн. Книга для недизайнерів.",
+        price: 70
+    },
+    {
+        author: "Алан Мур",
+        name: "Неономікон",
+        price: 70
+    },
+    {
+        author: "Террі Пратчетт",
+        name: "Рухомі картинки",
+        price: 40
+    },
+    {
+        author: "Анґус Гайленд",
+        name: "Коти в мистецтві",
+    }
+];
+
+let root = document.querySelector("#root");
+let ul = document.createElement("ul");
+root.append(ul);
+
+
+books.map(item => {
+
+    const {author, name, price} = item;
+
+    if (!!author && !!name && !!price) {
+        let ul = document.createElement("ul");
+        ul.innerHTML = `
+       <li>${author}</li>
+       <li>${name}</li>
+       <li>${price}</li>
+       `
+        root.append(ul);
+    }
+
+    try {
+        if (!author || !name || !price) {
+            throw new Error("The property is not in obj")
+        }
+
+    } catch (e) {
+        console.error(e.message)
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
