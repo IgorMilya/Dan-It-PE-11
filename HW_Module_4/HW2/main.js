00a10d81409a18fd7aca943b3d7fa6773b89e5ff
STRIX <imilay11@gmail.com>
2023-02-12 14:45:26 +0200

    Preparation for passing hw2

---
+++ HW_Module_4/HW2/main.js
@@ -0,0 +1,104 @@
+// let json2 = ' { "users":{"User1":{"income": "1000", "work": "Hostinger"},"User2":{"income2": "12", "work2": "Dan It"}} } ';
+// console.log(json2);
+//
+// const {users} = JSON.parse(json2);
+// const {User2} = users
+// const {work2} = User2
+
+// console.log(work2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const books = [
+//     {
+//         author: "Люсі Фолі",
+//         name: "Список запрошених",
+//         price: 70
+//     },
+//     {
+//         author: "Сюзанна Кларк",
+//         name: "Джонатан Стрейндж і м-р Норрелл",
+//     },
+//     {
+//         name: "Дизайн. Книга для недизайнерів.",
+//         price: 70
+//     },
+//     {
+//         author: "Алан Мур",
+//         name: "Неономікон",
+//         price: 70
+//     },
+//     {
+//         author: "Террі Пратчетт",
+//         name: "Рухомі картинки",
+//         price: 40
+//     },
+//     {
+//         author: "Анґус Гайленд",
+//         name: "Коти в мистецтві",
+//     }
+// ];
+//
+// let root = document.querySelector("#root");
+// let ul = document.createElement("ul");
+// root.append(ul);
+//
+//
+// books.map(item => {
+//     let li = document.createElement("li");
+//     const {author, name, price} = item;
+//
+//     if (!!author && !!name && !!price) {
+//         li.textContent = `${author} ${name} ${price}`
+//     } else {
+//         return;
+//     }
+//     ul.append(li);
+//
+//
+//     // if (!!author && !!name && !!price) {
+//     //     let ul = document.createElement("ul");
+//     //    ul.innerHTML = `
+//     //    <li>${author}</li>
+//     //    <li>${name}</li>
+//     //    <li>${price}</li>
+//     //    `
+//     //  root.append(ul);
+//     // }
+//
+//
+//
+//
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
