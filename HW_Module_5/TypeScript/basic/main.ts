1c938e4498f1bbdcad6c2a587e0c3842305ebadc
imilay11 <imilay11@gmail.com>
2023-04-19 01:43:17 +0300

    TS

--- HW_Module_5/TypeScript/basic/main.ts
+++ HW_Module_5/TypeScript/basic/main.ts
@@ -8,10 +8,9 @@ const greetUser = (): void => {
   alert("smth")
 }
 
-const list: Array<number> = [1,2,3,4]
-const x: [string, number] = ["a", 1, ]
-const y: Array<any> = ["a", 1, ]
-
+const list: Array<number> = [1, 2, 3, 4]
+const x: [string, number] = ["a", 1,]
+const y: Array<any> = ["a", 1,]
 
 enum D {
   i,
@@ -21,21 +20,77 @@ enum D {
   e
 }
 
-
-const create = (obj :object | null) :void => {}
+const create = (obj: object | null): void => {
+}
 
 create({obj: 1})
 
 type UserID = number
 
-const id :UserID = 1;
+const id: UserID = 1;
+
+const enum link {
+  youtube = "https://youtube.come",
+  facebook = "https://facebook.come",
+  instagram = "https://instagram.come",
+}
+
+const arr = [link.facebook, link.instagram]
+
+const createPassword = (name: string, age: number | string) => `${name}${age}`
+
+createPassword("ihor", 12)
+createPassword("ihor", "12")
+
+const createPassword2 = (name: string = "Max", age: number | string = 12) => `${name}${age}`
+createPassword2()
+
+
+const createPassword3 = (name: string, age?: number) => `${name}${age}`
+createPassword3("Jeck")
+
+
+const createPassword4 = (name: string, ...skills: Array<string>) => `${name}${skills}`
+createPassword4("Jeck")
+
+const createPassword5 = (name: string, age: number | string): string => `${name}${age}`
+const sum = (first: number, second: number): number => first + second
+const createObj = (): object => ({})
+
+
+type Person = {
+  name: string,
+  age: number,
+  nickName?: string,
+  getPass?: () => string
+}
+
+const user: Person = {
+  name: "Ihor",
+  age: 18,
+  nickName: "Menma"
+}
+
+const admin: Person = {
+  name: "Ihor",
+  age: 18,
+  getPass(){
+    return`${this.name}${this.age}`
+  }
+}
+
+const user2: { name: string, age: number } = {
+  name: "Ihor",
+  age: 18
+}
 
 
 
+class Admin {
 
-
-
-
+  constructor() {
+  }
+}
 
 
 
